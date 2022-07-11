e1209e8826c9663792a5a3e4f1ccb2f0dda811f5
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-07-11 23:04:23 +0530

    up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return(
+    <>
+      <h2>FakeShop</h2>
+        <div className="container">
+          <div className='box'>
+            <div className='content'>
+              <h5>Title</h5>
+              <p>Description</p>
+            </div>
+            <h1>Image</h1>
+          </div>
+          <div className='box'>
+            <div className='content'>
+              <h5>Title</h5>
+              <p>Description</p>
+            </div>
+            <h1>Image</h1>
+          </div>
+          <div className='box'>
+            <div className='content'>
+              <h5>Title</h5>
+              <p>Description</p>
+            </div>
+            <h1>Image</h1>
+          </div>
+          <div className='box'>
+            <div className='content'>
+              <h5>Title</h5>
+              <p>Description</p>
+            </div>
+            <h1>Image</h1>
+          </div>
+        </div>
+        
+    </>
+  )
 }
 
-export default App;
+export default App
