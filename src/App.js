c8cd860b77d32e71cf7f1edf20819a44b7a7c5be
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-07-11 23:57:36 +0530

    v1.0.0

--- src/App.js
+++ src/App.js
@@ -1,39 +1,45 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
+
+  // https://fakestoreapi.com/products
+
+  const [fake, setFake] = useState([]);
+  console.log(fake);
+
+  useEffect(() => {
+    fakestore();
+  }, [])
+
+  const fakestore = async() => {
+    const response = await fetch('https://fakestoreapi.com/products');
+    // console.log(response);
+    const jsonData = await response.json();
+    // console.log(jsonData);
+    setFake(jsonData);
+  }
+
+  // fakestore();
+
   return(
     <>
       <h2>FakeShop</h2>
         <div className="container">
-          <div className='box'>
-            <div className='content'>
-              <h5>Title</h5>
-              <p>Description</p>
-            </div>
-            <h1>Image</h1>
-          </div>
-          <div className='box'>
-            <div className='content'>
-              <h5>Title</h5>
-              <p>Description</p>
-            </div>
-            <h1>Image</h1>
-          </div>
-          <div className='box'>
-            <div className='content'>
-              <h5>Title</h5>
-              <p>Description</p>
-            </div>
-            <h1>Image</h1>
-          </div>
-          <div className='box'>
-            <div className='content'>
-              <h5>Title</h5>
-              <p>Description</p>
-            </div>
-            <h1>Image</h1>
-          </div>
+        {fake.map((values) => {
+          return(
+            <>
+              <div className='box'>
+                <div className='content'>
+                  <h5>{values.title}</h5>
+                  <p>{values.description}</p>
+                </div>
+                  <img src={values.image} alt='Product Image' />
+              </div>
+            </>
+          );
+        })}  
+          
         </div>
         
     </>
